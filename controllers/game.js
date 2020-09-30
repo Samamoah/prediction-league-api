2bfa8af0022c1b4c2a82904604a396dc0c45a6a6
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2020-09-30 00:56:32 +0000

    new gameweek

--- controllers/game.js
+++ controllers/game.js
@@ -84,7 +84,7 @@ module.exports = {
             currentMatchday: game.season.currentMatchday,
           };
         })
-        .filter((game) => game.matchday === 3)
+        .filter((game) => game.matchday === 4)
         .filter((game) => game.status !== 'IN_PLAY')
         .filter((game) => game.status !== 'PAUSED')
         .filter((game) => game.status !== 'FINISHED');
@@ -93,7 +93,7 @@ module.exports = {
       //console.log('here', req);
       res.json({
         confirmation: 'success',
-        matchday: 4,
+        matchday: 5,
         competition: competition.data.competition.name,
         data: unfinishedgames,
       });
