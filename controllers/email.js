8d850e1063ae09765b65746dc14ec3e4db1e7777
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2020-12-21 16:16:04 +0000

    Update email.js

--- controllers/email.js
+++ controllers/email.js
@@ -15,9 +15,9 @@ module.exports = {
               text: `
               Hello ${user.name},
 
-              Don't forget to predict Gameweek 21 games. Premier League games
+              Don't forget to predict Gameweek 22 games. Premier League games
               
-              Regards
+              Merry Christmas
               `,
               from: 'no-reply@predictionleague.com',
               to: user.email,
