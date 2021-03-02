7fbd6b0b0e2a934df0d4a81307ac9bf9aeb80b16
Samuel Tetteh <samuelamoahtetteh@gmail.com>
2021-03-02 14:14:02 +0000

    Update game.js

--- controllers/game.js
+++ controllers/game.js
@@ -98,7 +98,7 @@ module.exports = {
             currentMatchday: game.season.currentMatchday,
           };
         })        
-        .filter((game) => game.matchday === 27)
+        .filter((game) => game.matchday === 29)
         //.filter((game) => game.homeTeam === "SS Lazio" || game.homeTeam ===  "Club Atlético de Madrid" || game.homeTeam ===  "Borussia Mönchengladbach" || game.homeTeam ===  "Atalanta BC")
         .filter((game) => game.status !== 'IN_PLAY')
         .filter((game) => game.status !== 'PAUSED')
